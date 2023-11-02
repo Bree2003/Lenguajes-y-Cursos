6017ccb56a66640f5dd6f594b49608d64c7d4e2f
Bree2003 <breeale2003@gmail.com>
2023-11-02 19:07:28 -0300

    first view to dart

---
+++ Flutter/01-dart-intro/01_hello_world.dart
@@ -0,0 +1,11 @@
+void main() {
+    //  var myName = 'Brisa';
+    //  late final myName;
+    //  String myName = 'Brisa';
+
+    const myName = 'Brisa';
+
+    print('Hola $myName');
+    print('Hola ${myName.toUpperCase()}');
+    print('Hola ${ 1 + 1 }');
+}
