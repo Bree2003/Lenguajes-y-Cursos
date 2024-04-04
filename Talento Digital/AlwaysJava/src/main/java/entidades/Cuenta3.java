5ca258719123e93b180cf0f7bfda025ef6b58ca5
Bree2003 <breeale2003@gmail.com>
2024-04-03 22:06:57 -0300

    ejemplos con constructores, getter y setter , metodos e instanciacion de clases

---
+++ Talento Digital/AlwaysJava/src/main/java/entidades/Cuenta3.java
@@ -0,0 +1,12 @@
+package entidades;
+
+public class Cuenta3 {
+    public static void main(String[] args) {
+        Cuenta cuenta4 = new Cuenta("sasha", 100000);
+        cuenta4.toString();
+        System.out.println("los datos de la cuenta 4 es: "+ cuenta4);
+        Cuenta cuenta5 = new Cuenta("sasha", 100000);
+        cuenta5.toString();
+        System.out.println("los datos de la cuenta 5 es: "+ cuenta5);
+    }
+}
